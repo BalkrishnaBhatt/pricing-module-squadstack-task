188ab7d9cc71a6d3c1207535c6d1ccd8b6750a46
Balkrishna Bhatt <BalkrishnaBhatt@MacBook-Pro.local>
2021-07-11 01:25:08 +0530

    remove extras

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 import '@tailwindcss/ui/dist/tailwind-ui.css';
-import React, { useState } from 'react';
+import React from 'react';
 import 'react-pro-sidebar/dist/css/styles.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Pricing from './components/Pricing';
 import NoInternet from './components/NoInternet';
 import PriceModal from './components/PriceModal';
-import './css/fonts.css';
-import './css/style.css';
-const App = (props: any) => { 
-  let [isAuth, setIsAuth] = useState(false);
+
+const App = (props: any) => {
+
   return (
     <>
       <Router>
         <Route exact path="/">
           {
-            window.navigator.onLine ?  <Pricing /> : <NoInternet /> 
+            window.navigator.onLine ? <Pricing /> : <NoInternet />
           }
           {/* <PriceModal /> */}
         </Route>
